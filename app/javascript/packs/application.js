41b9769468909e8d4ceb0d3fe24a0f8155ac361e
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-08-23 10:22:29 +0100

    Upgrade to latest webpacker and reinstall

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,10 @@
 /* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
 
-import React from 'react';
-import ReactDOM from 'react-dom'
-import GufreBox from '../gufremagicbox'
-import Login from '../auth';
-
-console.log();
-let url = window.location.pathname;
-
-if(url === '/') {
-    ReactDOM.render(React.createElement(GufreBox, null), document.getElementById('gufrebox'));
-} else if(url === '/users/sign_in') {
-    ReactDOM.render(React.createElement(Login, null), document.getElementById('login'))
-}
+console.log('Hello World from Webpacker')
